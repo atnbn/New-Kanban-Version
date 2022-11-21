47ab7a09adef36cf0bc8bd5ba424473db3d5c9bd
atnbn <86778371+atnbn@users.noreply.github.com>
2022-11-21 15:06:40 +0100

    add forgot password

---
+++ js/forgot-password.js
@@ -0,0 +1,16 @@
+"use strict"
+const email = document.getElementById('email')
+const forgotBtn = document.getElementById('forgotBtn')
+
+
+
+forgotBtn.addEventListener('click', function () {
+    for (let user of users) {
+        if (user.email === email.value) {
+
+        }
+    }
+}
+)
+
+
